e6d0b233c443bee0beb615629b41a5d119c5b837
Aman Kalra <amankalra@Amans-MacBook-Pro.local>
2022-01-19 21:00:01 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import {
+  Card,
+  Grid,
+  CardMedia,
+  CardContent,
+  Typography,
+  Container,
+  CardHeader,
+  CardActions,
+  IconButton,
+  AppBar,
+  Toolbar,
+  listItemIconClasses,
+} from "@mui/material";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pokemon: [],
+      pokedetails: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=QiD1g5VohUdH3ov1DQiswSYcS6BZx8Vi64t6JrGW&count=10"
+      )
+      .then((result) => {
+        const items = result.data;
+        const pokemon = items.map((item, index) => {
+          return {
+            ...item,
+            liked: false,
+          };
+        });
+        this.setState({ pokemon: pokemon });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleLike = (item) => {
+    item.liked = !item.liked;
+
+    this.setState((prevState) => ({
+      ...prevState,
+      liked: !prevState.liked,
+    }));
+  };
+
+  render() {
+    console.log("hello", this.state.pokemon);
+
+    return (
+      <div className="div">
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h3">Spacetagram</Typography>
+          </Toolbar>
+        </AppBar>
+        {/* // <Container sx={{ margin: "50px 50px" }}> */}
+        <Grid className="grid" container spacing={3} padding={3} marginTop={4}>
+          {this.state.pokemon.map((item, index) => {
+            console.log("item", item.liked);
+            return (
+              <Grid item key={index} xs={6} sm={4} md={3}>
+                <Card raised>
+                  <CardMedia
+                    image={item.url}
+                    sx={{ height: "250px" }}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography variant="h6" fontWeight="bold" align="center">
+                      {item.title}
+                    </Typography>
+                    <Typography variant="subtitle1" align="center">
+                      {item.date}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <IconButton>
+                      {item.liked ? (
+                        <FavoriteIcon
+                          onClick={(item) => this.handleLike(item)}
+                          sx={{ color: "red" }}
+                        />
+                      ) : (
+                        <FavoriteBorderIcon />
+                      )}
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+        {/* // </Container> */}
+      </div>
+    );
+  }
 }
 
 export default App;
